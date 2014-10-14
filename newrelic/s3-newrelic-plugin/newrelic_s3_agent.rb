5c2699595f2cfe585475f50bd1d8fd15852a4cc2
Ben Patterson <bpatterson@edx.org>
2014-10-14 16:48:32 -0400

    Capture total object count for a specific bucket.

--- newrelic/s3-newrelic-plugin/newrelic_s3_agent.rb
+++ newrelic/s3-newrelic-plugin/newrelic_s3_agent.rb
@@ -3,25 +3,55 @@
 # An agent that collects metrics
 # From S3 metadata
 
+# Requires ruby >= 1.9
+
 require "rubygems"
 require "bundler/setup"
 require "newrelic_plugin"
+require "aws-sdk"
 
 module S3NewRelicAgent
 
-class S3Agent < NewRelic::Plugin::Agent::Base
-  # stuff
+    class Agent < NewRelic::Plugin::Agent::Base
 
-  def start_metric_collection_vars
+        agent_guid "org.edx.s3watcher"
+        agent_version "0.0.1"
+        agent_config_options :s3_access_key_id, :s3_secret_access_key, :s3_bucket_name, :s3_region
 
-  end
+        agent_human_labels("S3 Agent") { "sample" }
 
-  def poll_cycle
+        def setup_metrics
+            AWS.config(access_key_id: s3_access_key_id, secret_access_key: s3_secret_access_key, region: s3_region)
+            I18n.enforce_available_locales = false
 
-  end
 
-NewRelic::Plugin::Setup.install_agent # params?
+            @aws_s3 = AWS::S3.new
 
-NewRelic::Plugin::Run.setup_and_run
+        end
+
+        def get_s3_bucket_object_count
+
+            # Excluded for now but leaving abstraction for later:
+            # gathering stats for multiple buckets.
+
+            bucket = @aws_s3.buckets[s3_bucket_name]
+            return bucket.objects.count
+
+        end
+
+        def poll_cycle
+
+            report_metric "Number of bucket objects", "Objects", get_s3_bucket_object_count
+        end
+
+
+    end
+#    TODO: Test these two approaches to installing the agent
+#    NewRelic::Plugin::Setup.install_agent :s3, S3NewRelicAgent
+    NewRelic::Plugin::Config.config.agents.keys.each do |agent|
+        NewRelic::Plugin::Setup.install_agent agent, S3NewRelicAgent
+    end
+
+    NewRelic::Plugin::Run.setup_and_run
 
 end
